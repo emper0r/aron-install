aed3c2daea46704f17107d195dfa5f03d6023417
Tony Peña <antonio@ctime.it>
2016-02-28 14:21:12 +0100

    * Upgrade installer script
      + Adding pre-compiled packages for squid+ssl options
      + Update squid.conf for init on routing mode.
      + Update firewall by squid rules.

--- aronsetup.sh
+++ aronsetup.sh
@@ -7,28 +7,28 @@ if [ "$WHOAMI" = "$SU" ]; then
     echo "deb http://it.archive.ubuntu.com/ubuntu wily main restricted universe multiverse" > /etc/apt/sources.list
     echo "deb http://it.archive.ubuntu.com/ubuntu wily-updates main restricted universe multiverse" >> /etc/apt/sources.list
     echo "deb http://it.archive.ubuntu.com/ubuntu wily-backports main restricted universe multiverse" >> /etc/apt/sources.list
-    echo -n "Indirizzo MAC (in formato 00:00:00:00:00:00) del PC di gestione?: ";
-    read MAC;
+    echo -n "Indirizzo MAC (in formato 00:00:00:00:00:00) del PC di gestione?: "
+    read MAC
     echo
     while [[ ! "$MAC" =~ ^([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}$ ]]
     do
         clear
-        echo -n "L'indirizzo MAC e' sbagliato, inserici di nuovo: ";
-        read MAC;
+        echo -n "L'indirizzo MAC e' sbagliato, inserici di nuovo: "
+        read MAC
     done
-    echo -n "Username per GIT?: ";
+    echo -n "Username per GIT?: "
     read -s USERGIT
     echo
-    echo -n "Password per GIT?: ";
+    echo -n "Password per GIT?: "
     read -s PASSGIT
-    apt-get update;
-    apt-get -y -f dist-upgrade;
-    cd /usr/local/src;
+    apt-get update
+    apt-get -y -f dist-upgrade
+    cd /usr/local/src
     git clone http://$USERGIT:$PASSGIT@aron.ctimeapps.it/tony/aron-tools.git
     cd /usr/local/src/aron-tools
     export DEBIAN_FRONTEND=noninteractive
-    apt-get -y install squid3 python-mysqldb python-django python-pip python-crypto firehol apache2 pwgen sshpass \
-                       libapache2-mod-wsgi isc-dhcp-server libsodium-dev sudo hdparm ntp python-bcrypt mrtg snmpd;
+    apt-get -y install python-mysqldb python-django python-pip python-crypto firehol apache2 pwgen sshpass \
+                       libapache2-mod-wsgi isc-dhcp-server libsodium-dev sudo hdparm ntp python-bcrypt mrtg snmpd
     MYSQLPASS=`pwgen -s 32 -n 1`
     echo
     ARONPASS=`pwgen -s 32 -n 1`
@@ -36,34 +36,55 @@ if [ "$WHOAMI" = "$SU" ]; then
     debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQLPASS"
     debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQLPASS"
     apt-get -y install mysql-server
-    cd /usr/local/src;
-    git clone http://$USERGIT:$PASSGIT@aron.ctimeapps.it/tony/aron-web.git;
-    pip install singlemodeladmin;
-    pip install django-sizefield;
-    pip install libnacl;
-    pip install base58;
-    pip install iptools;
-    pip install django-tables2;
-    pip install pymysql;
-    git clone https://github.com/darklow/django-suit;
-    cd /usr/local/src/django-suit;
-    python setup.py install;
-    echo $MAC > /etc/firehol/mac_allow;
-    echo "www-data ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers;
-    echo "support ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers;
-    cd /usr/local/src/aron-web;
-    git checkout aron-proxy;
-    mysql -u root -h localhost --password=$MYSQLPASS -e "CREATE DATABASE aron;";
-    mysql -u root -h localhost --password=$MYSQLPASS -e "GRANT ALL PRIVILEGES ON aron.* TO 'aron'@'localhost' IDENTIFIED BY '$ARONPASS';";
-    mysql -u root -h localhost --password=$MYSQLPASS -e "FLUSH PRIVILEGES;";
+    cd /usr/local/src
+    git clone http://$USERGIT:$PASSGIT@aron.ctimeapps.it/tony/aron-web.git
+    pip install singlemodeladmin
+    pip install django-sizefield
+    pip install libnacl
+    pip install base58
+    pip install iptools
+    pip install django-tables2
+    pip install pymysql
+    git clone https://github.com/darklow/django-suit
+    cd /usr/local/src/django-suit
+    python setup.py install
+    echo $MAC > /etc/firehol/mac_allow
+    echo "www-data ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+    echo "support ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+    cd /usr/local/src/aron-web
+    git checkout aron-proxy
+    mysql -u root -h localhost --password=$MYSQLPASS -e "CREATE DATABASE aron;"
+    mysql -u root -h localhost --password=$MYSQLPASS -e "GRANT ALL PRIVILEGES ON aron.* TO 'aron'@'localhost' IDENTIFIED BY '$ARONPASS';"
+    mysql -u root -h localhost --password=$MYSQLPASS -e "FLUSH PRIVILEGES;"
     sed -i "s/CHANGEMAC/$MAC/g" /usr/local/src/aron-web/fixtures/init.sql
-    mysql -u aron -h localhost --database=aron --password=$ARONPASS < /usr/local/src/aron-web/fixtures/init.sql;
+    mysql -u aron -h localhost --database=aron --password=$ARONPASS < /usr/local/src/aron-web/fixtures/init.sql
     rm -f /usr/local/src/aron-web/fixtures/init.sql
     mv /usr/local/src/aron-tools/fixtures/config.py /usr/local/lib/python2.7/dist-packages/django_suit-0.2.15-py2.7.egg/suit/config.py
     mv /usr/local/src/aron-tools/fixtures/base.html /usr/local/lib/python2.7/dist-packages/django_suit-0.2.15-py2.7.egg/suit/templates/admin/base.html
-    mv /usr/local/src/aron-tools/fixtures/aron.conf /etc/apache2/sites-available/000-default.conf;
+    mv /usr/local/src/aron-tools/fixtures/aron.conf /etc/apache2/sites-available/000-default.conf
+    dpkg -i /usr/local/src/aron-tools/fixtures/libdb5.1_5.1.29-7ubuntu1_amd64.deb
+    dpkg -i /usr/local/src/aron-tools/fixtures/squid-langpack_20150704-1_all.deb
+    dpkg -i /usr/local/src/aron-tools/fixtures/squidclient_3.3.8-1ubuntu16_amd64.deb
+    dpkg -i /usr/local/src/aron-tools/fixtures/squid3-common_3.4.9_all.deb
+    dpkg -i /usr/local/src/aron-tools/fixtures/squid3_3.4.9_amd64.deb
+    /etc/init.d/squid3 stop
+    touch /etc/squid3/mime.conf
+    rm -rfv /var/cache/squid/
+    rm -f /etc/squid3/squid.conf
+    mkdir /var/cache/squid3
+    chown proxy:proxy /var/cache/squid3 -R
+    cp /usr/local/src/aron-tools/fixtures/init-daemon-squid3 /etc/init.d/squid3
     mv /usr/local/src/aron-tools/fixtures/squid.conf /etc/squid3/
     mv -f /usr/local/src/aron-tools/fixtures/log_db_daemon /usr/lib/squid3/log_db_daemon
+    chmod 755 /usr/lib/squid3/log_db_daemon
+    mv /usr/local/src/aron-toosl/fixtures/myCA.pem /etc/squid3/myCA.pem
+    mv /usr/local/src/aron-toosl/fixtures/myCA.der /etc/squid3/myCA.der
+    /usr/lib/squid3/ssl_crtd -c -s /var/lib/ssl_db/
+    chown proxy:proxy /var/lib/ssl_db/ -R
+    touch /var/log/squid3/cache.log
+    chmod 666 /var/log/squid3/cache.log /etc/squid3/mime.conf
+    chown proxy:proxy /var/log/squid3/cache.log
+    mv /etc/init.d/squid3.dpkg-new /etc/init.d/squid3
     mv /usr/local/src/aron-tools/fixtures/interfaces /etc/network/interfaces
     mv /usr/local/src/aron-toosl/fixtures/dhcpd.conf /etc/dhcp/dhcpd.conf
     mv /usr/local/src/aron-tools/fixtures/logfile-daemon_mysql.pl /usr/lib/squid3/
@@ -73,10 +94,6 @@ if [ "$WHOAMI" = "$SU" ]; then
     sed -i 's/NO/YES/g' /etc/default/firehol
     sed -i "s/CHANGE/$ARONPASS/g" /usr/local/src/aron-web/web/settings.py
     sed -i 's/80/8088/g' /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
-    mkdir /var/cache/squid3
-    /etc/init.d/squid3 stop
-    chown proxy:proxy /var/cache/squid3 -R
-    cp /usr/local/src/aron-tools/fixtures/init-daemon-squid3 /etc/init.d/squid3
     rm -rfv /usr/local/src/django-suit
     echo "nameserver 8.8.8.8" > /etc/resolv.conf
     echo "nameserver 8.8.4.4" >> /etc/resolv.conf
@@ -103,16 +120,12 @@ if [ "$WHOAMI" = "$SU" ]; then
     chmod +x /etc/rc.local
     find /etc/squid3/blacklists/ -type d -exec chmod 755 {} \;
     find /etc/squid3/blacklists/ -type f -exec chmod 666 {} \;
-    clear;
+    clear
     adduser support --quiet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --home /usr/local/src/aron-web/web/ --disabled-password --shell /usr/local/src/aron-web/web/support.py
     echo "support:support" | chpasswd
     adduser support www-data
     chown -R support:support /usr/local/src/aron-web/web/npyscreen/ /usr/local/src/aron-web/web/support.py
     echo "Making cache directory ... after finish the system will be rebooted"
-    mv /usr/local/src/aron-toosl/fixtures/myCA.pem /etc/squid3/myCA.pem
-    mv /usr/local/src/aron-toosl/fixtures/myCA.der /etc/squid3/myCA.der
-    /usr/lib/squid3/ssl_crtd -c -s /var/lib/ssl_db/
-    chown proxy:proxy /var/lib/ssl_db/ -R
     rm -rfv /usr/local/src/aron-tools
     /usr/sbin/squid3 -z &
     while true;
